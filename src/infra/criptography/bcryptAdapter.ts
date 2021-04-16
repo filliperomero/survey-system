b3067e2af91ec04081d1ef45dacc6854ae694ed7
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-15 21:33:19 -0300

    feat: add bcryptAdapter

---
+++ src/infra/criptography/bcryptAdapter.ts
@@ -0,0 +1,9 @@
+import bcrypt from 'bcrypt'
+
+import { Encrypter } from '../../data/protocols/encrypter'
+
+export class BcryptAdapter implements Encrypter {
+  async encrypt(value: string): Promise<string> {
+    return bcrypt.hash(value, 12)
+  }
+}
