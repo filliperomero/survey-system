fcd147647744de1059ac63d8540652bc77b9023f
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-15 22:28:49 -0300

    chore: add more functions to mongoHelper

--- src/infra/db/mongodb/helpers/mongoHelper.ts
+++ src/infra/db/mongodb/helpers/mongoHelper.ts
@@ -1,14 +1,26 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+
   async connect(url: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+    this.client = await MongoClient.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
   },
+
   async disconnect(): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map(collection: any): any {
+    const { _id, ...rest } = collection
+
+    return Object.assign({}, rest, { id: _id })
   }
 }
