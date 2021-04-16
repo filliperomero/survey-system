813786899fbec30c05c91c40d07c0d70ae33a379
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-16 12:27:38 -0300

    chore: change message when server is up

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,7 @@
 import app from './config/app'
 
-app.listen(process.env.PORT || 3333, () =>
-  console.log(`Server running at port ${process.env.PORT || 3333}`)
-)
+const port = process.env.PORT || 3333
+
+app.listen(port, () => {
+  console.log(`🟢 Server running on port: ${port} ✔`)
+})
