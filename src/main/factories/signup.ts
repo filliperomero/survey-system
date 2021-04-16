156a3c710908e293dfde481586bd7f36ab582b12
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-16 18:39:07 -0300

    refactor: ensure that signup controller is wrapped by logdecorator

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,11 +1,17 @@
+import { Controller } from '../../presentation/protocols'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/emailValidatorAdapter'
 import { DbAddAccount } from '../../data/useCases/addAccount/dbAddAccount'
 import { BcryptAdapter } from '../../infra/criptography/bcryptAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/accountRepository/accountMongoRepository'
+import { LogMongoRepository } from '../../infra/db/mongodb/logRepository/logMongoRepository'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const dbAddAccount = new DbAddAccount(new BcryptAdapter(), new AccountMongoRepository())
 
-  return new SignUpController(new EmailValidatorAdapter(), dbAddAccount)
+  return new LogControllerDecorator(
+    new SignUpController(new EmailValidatorAdapter(), dbAddAccount),
+    new LogMongoRepository()
+  )
 }
