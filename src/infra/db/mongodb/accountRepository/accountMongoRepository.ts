6a1d85e82e24353aad4877f98641eec1b13f82cd
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-15 22:32:44 -0300

    feat: add AccountMongoRepository

---
+++ src/infra/db/mongodb/accountRepository/accountMongoRepository.ts
@@ -0,0 +1,14 @@
+import { AddAccountRepository } from '../../../../data/protocols/addAccountRepository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/useCases/addAccount'
+import { MongoHelper } from '../helpers/mongoHelper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+
+    const result = await accountCollection.insertOne(accountData)
+
+    return MongoHelper.map(result.ops[0])
+  }
+}
