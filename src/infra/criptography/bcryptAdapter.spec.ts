0801883dd7534220c8a6e937d9317639764b433c
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-15 21:33:48 -0300

    test: add bcryptAdapter

---
+++ src/infra/criptography/bcryptAdapter.spec.ts
@@ -0,0 +1,37 @@
+import bcrypt from 'bcrypt'
+
+import { BcryptAdapter } from './bcryptAdapter'
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter()
+}
+
+describe('Bcrypt Adapter', () => {
+  it('should be able to call bcrypt with valid value', async () => {
+    const sut = makeSut()
+
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+
+    await sut.encrypt('any value')
+
+    expect(hashSpy).toHaveBeenCalledWith('any value', 12)
+  })
+
+  it('should be able to hash the value', async () => {
+    const sut = makeSut()
+
+    jest.spyOn(bcrypt, 'hash').mockResolvedValueOnce('hashed_value')
+
+    const hashedValue = await sut.encrypt('any value')
+
+    expect(hashedValue).toBe('hashed_value')
+  })
+
+  it('should be able to throw if bcrypt throws', async () => {
+    const sut = makeSut()
+
+    jest.spyOn(bcrypt, 'hash').mockRejectedValueOnce(new Error())
+
+    expect(() => sut.encrypt('any value')).rejects.toThrow()
+  })
+})
