99583764eb37702d5c0511d8e19f4ac3309b870e
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-16 00:20:23 -0300

    feat: ensure that body from requests are parsed correctly as json

---
+++ src/main/middlewares/bodyParser.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('', () => {
+  it('should be able to parse body as json', async () => {
+    expect(true).toBeTruthy()
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Joe doe' })
+      .expect({ name: 'Joe doe' })
+  })
+})
