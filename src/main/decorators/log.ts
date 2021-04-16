ee648e795d6a8fa49dd6f51e99c0cba65facdb7b
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-16 18:38:21 -0300

    feat: add log Decorator

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,16 @@
+import { LogErrorRepository } from '../../data/protocols/logErrorRepository'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+export class LogControllerDecorator implements Controller {
+  constructor(private controller: Controller, private logErrorRepository: LogErrorRepository) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+
+    if (httpResponse.statusCode === 500) {
+      await this.logErrorRepository.logError(httpResponse.body.stack)
+    }
+
+    return httpResponse
+  }
+}
