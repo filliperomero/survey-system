a63bd0a9502f4b74d79a6db337af15bb4175736f
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-18 19:07:44 -0300

    feat: add login route

--- src/main/routes/users.routes.test.ts
+++ src/main/routes/users.routes.test.ts
@@ -1,11 +1,16 @@
 import request from 'supertest'
+import { hash } from 'bcrypt'
 
 import { MongoHelper } from '@infra/db/mongodb/helpers/mongoHelper'
 import app from '../config/app'
+import { Collection } from 'mongodb'
+
+let accountCollection: Collection
 
 describe('Users Route', () => {
   beforeEach(async () => {
-    await (await MongoHelper.getCollection('accounts')).deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   beforeAll(async () => {
@@ -25,6 +30,26 @@ describe('Users Route', () => {
           email: 'joe.doe@mail.com',
           password: '123456',
           passwordConfirmation: '123456'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    it('should be able to return 200 on login', async () => {
+      const password = await hash('123456', 12)
+
+      await accountCollection.insertOne({
+        name: 'Joe Doe',
+        email: 'joe.doe@mail.com',
+        password
+      })
+
+      await request(app)
+        .post('/api/users/login')
+        .send({
+          email: 'joe.doe@mail.com',
+          password: '123456'
         })
         .expect(200)
     })
--- src/main/routes/users.routes.ts
+++ src/main/routes/users.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/expressRouteAdapter'
 import { makeSignUpController } from '../factories/signup/signupFactory'
+import { makeLoginController } from '../factories/login/loginFactory'
 
 export default (router: Router): void => {
   router.post('/users/signup', adaptRoute(makeSignUpController()))
+  router.post('/users/login', adaptRoute(makeLoginController()))
 }
