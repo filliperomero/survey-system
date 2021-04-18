1b45e59058e2c5ebfe137253ea1063b4886b1a51
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-18 14:03:55 -0300

    feat: add compare function to BcryptAdapter

--- src/infra/criptography/bcryptAdapter.ts
+++ src/infra/criptography/bcryptAdapter.ts
@@ -1,9 +1,13 @@
 import bcrypt from 'bcrypt'
 
-import { Hasher } from '@data/protocols/criptography/hasher'
+import { Hasher, HashComparer } from '@data/protocols/criptography'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   async hash(value: string): Promise<string> {
     return bcrypt.hash(value, 12)
+  }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(value, hash)
   }
 }
