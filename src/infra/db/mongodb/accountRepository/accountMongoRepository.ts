3d3b1342154250ba5d051070e437c938d8caf23b
Fillipe Romero <fillipe.romero@gmail.com>
2021-04-18 16:01:41 -0300

    feat: add loadByEmail function to AccountMongoRepository

--- src/infra/db/mongodb/accountRepository/accountMongoRepository.ts
+++ src/infra/db/mongodb/accountRepository/accountMongoRepository.ts
@@ -1,14 +1,24 @@
-import { AddAccountRepository } from '@data/protocols/db/addAccountRepository'
+import { LoadAccountByEmailRepository, AddAccountRepository } from '@data/protocols/db'
 import { AccountModel } from '@domain/models/account'
 import { AddAccountModel } from '@domain/useCases/addAccount'
 import { MongoHelper } from '../helpers/mongoHelper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
 
     const result = await accountCollection.insertOne(accountData)
 
     return MongoHelper.map(result.ops[0])
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+
+    const account = await accountCollection.findOne({ email })
+
+    if (!account) return null
+
+    return account && MongoHelper.map(account)
   }
 }
